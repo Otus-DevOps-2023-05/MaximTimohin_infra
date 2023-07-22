cfe62051dc5e720a8005f863a1d35cc47aebea5d
MaximTimohin <timohin.m.v@mail.ru>
2023-07-22 12:34:42 +0300

    Terraform-2

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_version = ">= 1.1.6"
+  required_version = ">= 0.13.0"
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.87.0"
+      version = "0.94.0"
     }
   }
 }
-
 resource "yandex_compute_instance" "db" {
   name = "reddit-db"
   labels = {
