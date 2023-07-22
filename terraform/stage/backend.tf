4c2a4b3474ca113c8c4306c70fe439d43ab4d585
MaximTimohin <timohin.m.v@mail.ru>
2023-07-22 12:23:04 +0300

    Terraform-2

---
+++ terraform/stage/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "otus-devops-reddit-busket"
+    region   = "ru-central1"
+    key      = "stage/terraform.tfstate"
+    access_key = "YCAJEf6wHRgl1sTEcaFRiWS2b"
+    secret_key = "YCN1QFyhubLKnsW5xirEk_358fTZ425tH4G9Lr9H"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
